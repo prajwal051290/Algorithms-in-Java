8189785d9b0a6b42891591ceb3aed93dd41aa2ba
prajwal051290 <pk.kondawar@gmail.com>
2016-08-29 21:45:56 -0700

    Logic of Size of Binary Tree

--- Tree/sizeOfTree.java
+++ Tree/sizeOfTree.java
@@ -1,3 +1,7 @@
+// Logic: Size of left subtree + size of right subtree + 1
+// When recursion reaches null, it returns 0 from left an both sides. 1 is added for that current node.
+// Then these values are passed up the stack and ultimately to calling function.
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
