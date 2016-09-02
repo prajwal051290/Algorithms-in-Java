717cc55e329009dd86f802489d1813fd65dd3770
prajwal051290 <pk.kondawar@gmail.com>
2016-09-01 21:23:05 -0700

    Swap Linked list nodes in pair

---
+++ LinkedList/SwapNodesInPair.java
@@ -0,0 +1,40 @@
+/*Given a linked list, swap every two adjacent nodes and return its head.
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        
+        if (head==null)
+            return head;
+            
+        ListNode curr = head;
+        ListNode prev = head;
+        
+        if (head.next!=null)
+            head = head.next;
+        else
+            return head;
+            
+        while(curr!=null && curr.next!=null){
+            prev.next = curr.next;
+            ListNode temp = curr.next.next;
+            curr.next.next = curr;
+            curr.next = temp;
+            prev = curr;
+            curr = curr.next;
+        }
+        return head;
+    }
+}
