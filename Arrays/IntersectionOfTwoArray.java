7d670b68139d2d93b64e1efdf3d98591b1f744b7
prajwal051290 <pk.kondawar@gmail.com>
2016-09-13 11:52:16 -0700

    Intersection of Two Arrays

---
+++ Arrays/IntersectionOfTwoArray.java
@@ -0,0 +1,36 @@
+/*Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.*/
+
+/*
+ * Logic: Create hash set of nums1, Now, while looping over nums2, check if it is present in set if yes then add to result.
+ * Using set to avoid duplicates
+ */
+
+import java.util.*;
+
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}
