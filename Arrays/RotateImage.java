c70576e70f31a653fca9a230434d72904085e14d
prajwal051290 <pk.kondawar@gmail.com>
2016-10-01 14:20:09 -0700

    Rotate 2D matrix in place

--- Arrays/RotateImage.java
+++ Arrays/RotateImage.java
@@ -7,8 +7,12 @@ Could you do this in-place?*/
 
 /*
  * Logic:
- * Find pattern on notebook, you will know i&j for result matrix.
- * Time - o(n^2), space - O(n^2) 
+ * 1. Find pattern on notebook, you will know i&j for result matrix.
+ * Time - o(n^2), space - O(n^2)
+ * 
+ * 2. Transpose + reverse row by row
+ * Time - O(n^2), Space - O(1)
+ *  
  */
 
 public class Solution {
@@ -26,6 +30,34 @@ public class Solution {
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 matrix[i][j] = temp[i][j];
+            }
+        }
+    }
+}
+
+----------------------------------------------------------------
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        for(int i=0; i<rows; i++){
+            for(int j=i+1; j<cols; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int i=0; i<rows; i++){
+            int dummyCol = cols-1;
+            for(int j=0; j<cols/2; j++){
+                int temp = matrix[i][dummyCol];
+                matrix[i][dummyCol] = matrix[i][j];
+                matrix[i][j] = temp;
+                dummyCol--;
             }
         }
     }
