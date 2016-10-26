1329ff5746b6413d91bf5a7e40bb6f036f5fe91d
prajwal051290 <pk.kondawar@gmail.com>
2016-10-26 01:01:43 -0700

    Added problem description and logic for staircase problem

--- Dynamic Programming/StaircaseReachTop.java
+++ Dynamic Programming/StaircaseReachTop.java
@@ -1,3 +1,16 @@
+/*
+ * Problem Description:
+ * You can climb 1 step or 2 steps of a staircase. Find the total no of ways in which you can reach the to the top.
+ * 
+ * Logic:
+ * This is exactly fibonnacci series.
+ * Formula: f(n) = f(n-1) + f(n-2).
+ * 
+ * Modification: if we you can climp 1, 2 or 3 step then formula becomes
+ * Formula: f(n) = f(n-1) + f(n-2) + f(n-3) and so on...
+ *  
+ */
+
 class stairs
 {
 	static int countWays(int n) {
